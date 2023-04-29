017498fd3bc73953b2563f071b95f2e9a8211f16
Ulisse Mini <ulisse.mini@gmail.com>
2023-04-28 22:36:09 -0700

    Add gradio playground

---
+++ demo.py
@@ -0,0 +1,38 @@
+# %%
+
+import torch
+import numpy as np
+import gradio as gr
+from PIL import Image
+from transformers import CLIPProcessor, CLIPModel
+
+# Load models
+# %%
+
+trm_model = torch.nn.Linear(512, 1)
+trm_model.load_state_dict(torch.load("model.pt"))
+trm_model.eval()
+
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").eval()
+processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+# %%
+
+def rateme(input_img):
+    # TODO: figure out what bullshit clip is doing
+    image = Image.fromarray(input_img)
+    inputs = processor(images=image, return_tensors="pt", padding=True)
+    image_features = model.get_image_features(**inputs)
+    rating = trm_model(image_features).item()
+    return f'you are a {rating:.1f}'
+
+
+# %%
+# Test it
+
+# rateme(np.random.rand(3, 224, 224))
+# shape is arbitrary
+# %%
+
+demo = gr.Interface(rateme, gr.Image(), "text")
+demo.launch()
