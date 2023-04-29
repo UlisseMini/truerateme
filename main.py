161ab0a17b14b2500b53a0dc207210236903f054
Ulisse Mini <ulisse.mini@gmail.com>
2023-04-28 21:06:31 -0700

    Add validation set, remove garbage from EOF

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 # Get all the vectors and embed them using clip
 # %%
 
-def clean_dataset(dataset):
-    return [(emb, rating) for emb, rating in dataset if not torch.isnan(emb).any() and not np.isnan(rating).any()]
+def remove_nan(dataset):
+    return [(emb, rating) for emb, rating in dataset if not torch.isnan(emb).any() and not np.torch(rating).any()]
 
 
 def load_dataset():
@@ -40,49 +40,72 @@ def load_dataset():
         # image = torch.tensor(np.array(image), device='mps') # TODO
         inputs = processor(images=image, return_tensors="pt", padding=True)
         image_features = model.get_image_features(**inputs)
-        rating = np.median(ratings)
+        rating = torch.tensor(np.median(ratings), dtype=torch.float32)
         dataset.append((image_features, rating))
 
+    dataset = remove_nan(dataset)
     return dataset
 
 
 if not os.path.exists('dataset.pt'):
     dataset = load_dataset()
-    dataset = clean_dataset(dataset)
     torch.save(dataset, "dataset.pt")
 else:
     dataset = torch.load('dataset.pt')
 
 
 # %%
+# Create train and test sets
+
+train_size = int(0.8 * len(dataset))
+test_size = len(dataset) - train_size
+train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+# %%
 # Create a dataloader for minibatching
 
 batch_size = 32
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
 
 # %%
-# Train linear model on clip vectors to predict rating
+
+def mean_loss(model, dataloader):
+    total_loss = 0.
+    with torch.no_grad():
+        for image_features, rating in dataloader:
+            prediction = model(image_features)
+            loss = ((prediction.squeeze() - rating)**2).mean()
+            total_loss += loss.item()
+
+    return total_loss / len(dataloader)
+
+
+# %%
+# Initialize model
 
 trm_model = torch.nn.Linear(512, 1)
-
 optim = torch.optim.Adam(trm_model.parameters(), lr=0.01)
+
+# %%
+# Train model
 
 epochs = 1000
 pbar = tqdm(total=epochs)
 for epoch in range(epochs):
-    total_loss = 0.
-    for image_features, rating in dataloader:
-        rating = rating.to(torch.float32)
+    for image_features, rating in train_dataloader:
         optim.zero_grad()
         prediction = trm_model(image_features)
         # loss = torch.nn.functional.mse_loss(prediction, rating.unsqueeze(1))
-        loss = ((prediction - rating.unsqueeze(1))**2).mean()
+        loss = ((prediction.squeeze() - rating)**2).mean()
         loss.backward()
         optim.step()
 
-        total_loss += loss.item()
+    # Evaluate
+    test_loss = mean_loss(trm_model, test_dataset)
+    train_loss = mean_loss(trm_model, train_dataset)
 
-    pbar.set_description("Loss: {:.4f}".format(total_loss / batch_size))
+    pbar.set_description(f"test_loss: {test_loss:.4f} train_loss {train_loss:.4f}")
     pbar.update(1)
 
 
